41aacc0a6ee496a7c79a79509c4765ea6c57d1d2
nampdn <nampdn@protonmail.com>
2020-01-17 14:13:35 +0700

    Initial Commit

---
+++ Bara StudioTests/Bara_StudioTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bara_StudioTests.swift
+//  Bara StudioTests
+//
+//  Created by VGM Builder on 1/17/20.
+//  Copyright © 2020 BaraJS. All rights reserved.
+//
+
+import XCTest
+@testable import Bara_Studio
+
+class Bara_StudioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
